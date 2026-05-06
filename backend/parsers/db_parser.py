9b456c6a888502cde302a0f73b5db2d74291af7f
loicbige <loic.bige@univ-tlse3.fr>
2026-05-06 15:48:27 +0200

    fix : parse_db.py now get data from log_data

--- backend/parsers/db_parser.py
+++ backend/parsers/db_parser.py
@@ -1,22 +1,30 @@
 from datetime import datetime
-import sqlite3
+import sqlite3, json
+
+
 
 
 def parse_shearwater(file_path: str):
     db = sqlite3.connect(file_path)
     db.row_factory = sqlite3.Row
-    rows = db.execute("SELECT * FROM dive_details").fetchall()
+    rows_dive_details = db.execute("SELECT * FROM dive_details").fetchall()
+    calculated = db.execute("SELECT * FROM log_data").fetchall()
     db.close()
 
-    result = []
-    for r in rows:
-        result.append({
+    log_date_dict = {}
+    dive_details = []
+    
+    for l in calculated:
+        log_date_dict[l["log_id"]] = json.loads(l["calculated_values_from_samples"])
+    for r in rows_dive_details:
+        logs = log_date_dict.get(r["DiveId"],{})
+        dive_details.append({
             "date": datetime.strptime(r["DiveDate"], "%Y-%m-%d %H:%M:%S") if r["DiveDate"] else None,
             "max_depth_m": float(r["Depth"].replace(",",".")) if  r["Depth"] else None,
-            "avg_depth_m": r["AverageDepth"],
-            "avg_temp_c": r["AverageTemp"],
-            "min_temp_c": r["MinTemp"],
-            "max_temp_c": r["MaxTemp"],
+            "avg_depth_m": logs.get("AverageDepth"),
+            "avg_temp_c": logs.get("AverageTemp"),
+            "min_temp_c": logs.get("MinTemp"),
+            "max_temp_c": logs.get("MaxTemp"),
             "duration_s": r["DiveLengthTime"],
             "site": r["Site"],
             "location": r["Location"],
@@ -26,6 +34,6 @@ def parse_shearwater(file_path: str):
             "notes": r["Notes"],
             "weight_kg": float(r["Weight"].replace("kg", "").strip()) if r["Weight"] else None
         })
-    return result
+    return dive_details
     
 
